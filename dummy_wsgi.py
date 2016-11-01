4e9bf105f4e7a616a4096fc4f450615409bc0f9a
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2016-11-01 20:40:07 +0100

    use clf

--- dummy_wsgi.py
+++ dummy_wsgi.py
@@ -2,28 +2,24 @@ import flask
 
 app = flask.Flask(__name__)
 
-print('barfoo')
 
-@app.route('/api/v1/<pid>/<model_id>/predict', methods=['POST'])
+@app.route('/v1/<pid>/<model_id>/predict', methods=['POST'])
 def predict(*args, **kw):
-    try:
-        print('foobar')
-        content = flask.request.get_data()
-        n_rows = len(content.split('\n'))
+    content = flask.request.get_data()
+    n_rows = len(content.split('\n')) - 2
+    print(n_rows)
 
-        resp = {'execution_time': 0.0,
-                'predictions': [{'prediction': float(i), 'row_id': i}
-                                for i, _ in enumerate(xrange(n_rows))],
-                'task': 'Regression',
-                }
+    resp = {'execution_time': 0.0,
+            'predictions': [{'prediction': '1.0', 'row_id': i,
+                             'class_probabilities': {'1.0': 0.5, '0.0': 0.5}}
+                            for i, _ in enumerate(xrange(n_rows))],
+            'task': 'Regression',
+            }
 
-        return flask.jsonify(**resp)
-    except Exception as e:
-        print(e)
-        raise
+    return flask.jsonify(**resp)
 
 
-@app.route('/ping', methods=['GET'])
+@app.route('/v1/ping', methods=['GET'])
 def ping():
     print('ping')
     return flask.jsonify(foo='bar')
